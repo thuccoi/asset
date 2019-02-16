b130be90dcc92a77c7b358b79abfaf681888aee0
Nguyen The Thuc <thucfami@gmail.com>
2019-02-16 11:50:48 +0700

    tree

--- iframe/tree/js/script.js
+++ iframe/tree/js/script.js
@@ -169,12 +169,7 @@ $(document).ready(function () {
         id_padre = 1;
     }
 
-    var dataJSON = [
-        {"id": "1", "parentid": "20", "name": "Presidencia"},
-        {"id": "2", "parentid": "1", "name": "Asesoría"},
-        {"id": "3", "parentid": "1", "name": "Área de "},
-        {"id": "4", "parentid": "2", "name": "Área de "}
-    ];
+    var dataJSON = [];
 
     if (typeof parent.dataJSON !== 'undefined') {
 
