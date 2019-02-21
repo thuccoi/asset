794275e107bfc56d0a0b21000f5917e748e86714
Nguyen The Thuc <thucfami@gmail.com>
2019-02-21 17:24:15 +0700

    show member log role

--- js/assignment/member.js
+++ js/assignment/member.js
@@ -8,12 +8,14 @@ var AssignmentMember = new function __AssignmentMember() {
 
                 TAMI.confirm.show("Thực hiện <b>hủy bỏ</b> vai trò của thành viên này", function () {
                     TAMI.helper.ajax('.assignment', '/assignment/member/remove-role', 'POST', {member_id: memberid, role_id: roleid}, function (data) {
+                        AssignmentMember.showIndexLog('#js-member-activitys', TAMI.pagedata.appid);
                     });
                 });
 
             } else { //else
                 TAMI.confirm.show("Thực hiện <b>bổ nhiệm</b> vai trò cho thành viên này", function () {
                     TAMI.helper.ajax('.assignment', '/assignment/member/add-role', 'POST', {member_id: memberid, role_id: roleid}, function (data) {
+                        AssignmentMember.showIndexLog('#js-member-activitys', TAMI.pagedata.appid);
                     });
                 });
             }
